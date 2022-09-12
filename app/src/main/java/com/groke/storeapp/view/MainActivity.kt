7e62f4f9c37a43dabf59426e22ffab04de33e00b
akamorra <lmkorigin@gmail.com>
2022-09-12 18:55:22 +0300

    ViewModel, Application state added

--- app/src/main/java/com/groke/storeapp/view/MainActivity.kt
+++ app/src/main/java/com/groke/storeapp/view/MainActivity.kt
@@ -1,8 +1,7 @@
 package com.groke.storeapp.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentManager
+import androidx.appcompat.app.AppCompatActivity
 import com.groke.storeapp.R
 import com.groke.storeapp.databinding.ActivityMainBinding
 
